131b3f306b51037a70752909c8a09b126da11aba
Archiolli <joao.roberto_2001@hotmail.com>
2025-03-11 19:25:23 -0300

    change app imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,22 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
-import { CategoryController } from 'app/category/category.controller';
+import { CategoryController } from './app/category/category.controller';
 import { PrismaService } from '../prisma/prisma.service';
-import { CategoryModule } from 'app/category/category.module';
+import { CategoryModule } from './app/category/category.module';
 import { AuthController } from 'auth/auth.controller';
-import { CategoryService } from 'app/category/category.service';
+import { CategoryService } from './app/category/category.service';
 import { AuthService } from 'auth/auth.service';
-import { UserService } from 'app/user/user.service';
-import { RestaurantService } from 'app/restaurant/restaurant.service';
-import { UserController } from 'app/user/user.controller';
-import { RestaurantController } from 'app/restaurant/restaurant.controller';
-import { RestaurantModule } from 'app/restaurant/restaurant.module';
+import { UserService } from './app/user/user.service';
+import { RestaurantService } from './app/restaurant/restaurant.service';
+import { UserController } from './app/user/user.controller';
+import { RestaurantController } from './app/restaurant/restaurant.controller';
+import { RestaurantModule } from './app/restaurant/restaurant.module';
 import { JwtModule } from '@nestjs/jwt';
-import { UserModule } from 'app/user/user.module';
-import { AuthModule } from 'auth/auth.module';
-import { LocationController } from 'app/location/location.controller';
-import { LocationModule } from 'app/location/location.module';
-import { LocationService } from 'app/location/location.service';
+import { UserModule } from './app/user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { LocationController } from './app/location/location.controller';
+import { LocationModule } from './app/location/location.module';
+import { LocationService } from './app/location/location.service';
 
 
 @Module({
