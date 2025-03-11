d4068d900266f9500d7c83d09a04cf8e8842c6cf
Archiolli <joao.roberto_2001@hotmail.com>
2025-03-11 19:37:38 -0300

    change @ imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,26 @@
+import { CategoryController } from '@app/category/category.controller';
 import { Module } from '@nestjs/common';
-import { PrismaModule } from '../prisma/prisma.module';
-import { CategoryController } from './app/category/category.controller';
-import { PrismaService } from '../prisma/prisma.service';
-import { CategoryModule } from './app/category/category.module';
-import { CategoryService } from './app/category/category.service';
-import { UserService } from './app/user/user.service';
-import { RestaurantService } from './app/restaurant/restaurant.service';
-import { UserController } from './app/user/user.controller';
-import { RestaurantController } from './app/restaurant/restaurant.controller';
-import { RestaurantModule } from './app/restaurant/restaurant.module';
-import { JwtModule } from '@nestjs/jwt';
-import { UserModule } from './app/user/user.module';
-import { AuthModule } from './auth/auth.module';
-import { LocationController } from './app/location/location.controller';
-import { LocationModule } from './app/location/location.module';
-import { LocationService } from './app/location/location.service';
+import { PrismaModule } from 'prisma/prisma.module';
 import { AuthController } from './auth/auth.controller';
+import { UserController } from '@app/user/user.controller';
+import { RestaurantController } from '@app/restaurant/restaurant.controller';
+import { LocationController } from '@app/location/location.controller';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from '@app/user/user.module';
+import { JwtModule } from '@nestjs/jwt';
+import { RestaurantModule } from '@app/restaurant/restaurant.module';
+import { CategoryModule } from '@app/category/category.module';
+import { LocationModule } from '@app/location/location.module';
+import { PrismaService } from 'prisma/prisma.service';
+import { CategoryService } from '@app/category/category.service';
 import { AuthService } from './auth/auth.service';
-
+import { UserService } from '@app/user/user.service';
+import { RestaurantService } from '@app/restaurant/restaurant.service';
+import { LocationService } from '@app/location/location.service';
 
 @Module({
-  imports: [PrismaModule, CategoryModule, AuthModule, UserModule, JwtModule, RestaurantModule, CategoryModule, LocationModule],
-  controllers: [CategoryController, AuthController, UserController, RestaurantController, CategoryController, LocationController],
-  providers: [PrismaService, CategoryService, AuthService, UserService, RestaurantService, CategoryService, LocationService],
+  imports: [PrismaModule, AuthModule, UserModule, JwtModule, RestaurantModule, CategoryModule, LocationModule],
+  controllers: [CategoryController, AuthController, UserController, RestaurantController, LocationController],
+  providers: [PrismaService, CategoryService, AuthService, UserService, RestaurantService, LocationService],
 })
 export class AppModule { }
