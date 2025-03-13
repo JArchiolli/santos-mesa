68faafc0716b1c6efb8b75c477d41793b54af9c2
Archiolli <joao.roberto_2001@hotmail.com>
2025-03-13 18:13:12 -0300

    aaaa

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,18 +5,18 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth/auth.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { PrismaService } from '../prisma/prisma.service';
-import { UserModule } from '@/app/user/user.module';
-import { RestaurantModule } from '@/app/restaurant/restaurant.module';
-import { CategoryModule } from '@/app/category/category.module';
-import { LocationModule } from '@/app/location/location.module';
-import { CategoryController } from '@/app/category/category.controller';
-import { UserController } from '@/app/user/user.controller';
-import { RestaurantController } from '@/app/restaurant/restaurant.controller';
-import { LocationController } from '@/app/location/location.controller';
-import { CategoryService } from '@/app/category/category.service';
-import { UserService } from '@/app/user/user.service';
-import { RestaurantService } from '@/app/restaurant/restaurant.service';
-import { LocationService } from '@/app/location/location.service';
+import { UserModule } from './app/user/user.module';
+import { RestaurantModule } from './app/restaurant/restaurant.module';
+import { CategoryModule } from './app/category/category.module';
+import { LocationModule } from './app/location/location.module';
+import { CategoryController } from './app/category/category.controller';
+import { UserController } from './app/user/user.controller';
+import { RestaurantController } from './app/restaurant/restaurant.controller';
+import { LocationController } from './app/location/location.controller';
+import { CategoryService } from './app/category/category.service';
+import { UserService } from './app/user/user.service';
+import { RestaurantService } from './app/restaurant/restaurant.service';
+import { LocationService } from './app/location/location.service';
 
 @Module({
   imports: [PrismaModule, AuthModule, UserModule, JwtModule, RestaurantModule, CategoryModule, LocationModule],
