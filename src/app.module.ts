3105b544f6e1c4247a7a0e6bcac236390d1f20e6
Archiolli <joao.roberto_2001@hotmail.com>
2025-03-13 18:48:26 -0300

    nnnnnn

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { JwtModule } from '@nestjs/jwt';
 import { PrismaModule } from '../prisma/prisma.module';
-import { UserModule } from './app/user/user.module';
-import { RestaurantModule } from './app/restaurant/restaurant.module';
-import { CategoryModule } from './app/category/category.module';
-import { LocationModule } from './app/location/location.module';
+import { UserModule } from '@/app/user/user.module';
+import { RestaurantModule } from '@/app/restaurant/restaurant.module';
+import { CategoryModule } from '@/app/category/category.module';
+import { LocationModule } from '@/app/location/location.module';
 
 @Module({
   imports: [PrismaModule, AuthModule, UserModule, JwtModule, RestaurantModule, CategoryModule, LocationModule],
