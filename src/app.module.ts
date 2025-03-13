5b67f406e4df4587de7b2d86aa4c5e8c9b6c67ca
Archiolli <joao.roberto_2001@hotmail.com>
2025-03-12 21:43:04 -0300

    a

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,22 @@
 import { Module } from '@nestjs/common';
-import { CategoryController } from '@app/category/category.controller';
 import { AuthController } from './auth/auth.controller';
-import { UserController } from '@app/user/user.controller';
-import { RestaurantController } from '@app/restaurant/restaurant.controller';
-import { LocationController } from '@app/location/location.controller';
 import { AuthModule } from './auth/auth.module';
-import { UserModule } from '@app/user/user.module';
 import { JwtModule } from '@nestjs/jwt';
+import { AuthService } from './auth/auth.service';
+import { PrismaModule } from '../prisma/prisma.module';
+import { PrismaService } from '../prisma/prisma.service';
+import { UserModule } from '@app/user/user.module';
 import { RestaurantModule } from '@app/restaurant/restaurant.module';
 import { CategoryModule } from '@app/category/category.module';
 import { LocationModule } from '@app/location/location.module';
+import { CategoryController } from '@app/category/category.controller';
+import { UserController } from '@app/user/user.controller';
+import { RestaurantController } from '@app/restaurant/restaurant.controller';
+import { LocationController } from '@app/location/location.controller';
 import { CategoryService } from '@app/category/category.service';
-import { AuthService } from './auth/auth.service';
 import { UserService } from '@app/user/user.service';
 import { RestaurantService } from '@app/restaurant/restaurant.service';
 import { LocationService } from '@app/location/location.service';
-import { PrismaModule } from '../prisma/prisma.module';
-import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   imports: [PrismaModule, AuthModule, UserModule, JwtModule, RestaurantModule, CategoryModule, LocationModule],
